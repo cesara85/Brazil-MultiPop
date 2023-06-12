9afc9e24d5a4912f4df27c36d998b0038460ccd7
cesara85 <cesarmcs@gmail.com>
2023-06-12 08:23:59 -0300

    fillstatespace files

--- fillstatespace.r
+++ fillstatespace.r
@@ -3,6 +3,14 @@ library(magrittr)
 # dttosave
 # [1] "sxdt"   "asfrdt" "emrdt" "imrdt"  "idmrdt" (in) "odmrdt" (out) "popdt"  "propdt" "srbdt"  
 
+### files with data
+read_csv("data/population/popdt2.csv")
+read_csv("data/mortality/sxdt2.csv")
+read_csv("data/fertility/asfrdt2.csv")
+read_csv("data/education/propdt2.csv")
+read_csv("data/migration/idmrdt2.csv")
+read_csv("data/migration/odmrdt2.csv")
+
 
 if(F) {#newSSP
   # stop()
@@ -63,6 +71,7 @@ data1 %<>%
   rename(region = area) %>% mutate(region = as.character(region)) %>% 
   select(region, Time, sex, edu, agest, pop)
 popdt2<-full_join(popdt, data1)
+write_csv(popdt2,"data/population/popdt2.csv")
 
 ################## not used 
 setDT(data1)
@@ -106,6 +115,8 @@ sxdt2<-full_join(sxdt,data2)
 sxdt2 %<>%
   mutate(nSx = case_when(nSx>=1~1.0000, TRUE~nSx))
 
+write_csv(sxdt2,"data/mortality/sxdt2.csv")
+
 # asfrdt ------------------------------------------------------------------
 
 data3<-read_csv("data/fertility/ASFR2010_2060.csv")
@@ -117,6 +128,8 @@ data3 %<>%
   select(region, Time, edu, agest, asfr)
 
 asfrdt2<-full_join(asfrdt,data3)
+
+write_csv(asfrdt2,"data/fertility/asfrdt2.csv")
 
     # #Note: for the first few periods, population by mother's edu is not available
     # #asfrs (7)
@@ -190,397 +203,47 @@ data4 %<>%
   rename(edu = education) %>% 
   mutate(region = as.character(area)) %>% 
   select(region, Time, sex, edu, agest, prjpropfinal)
-propdt2<-full_join(propdt,data4)    
+propdt2<-full_join(propdt,data4)
 
-###### not used    
-    if(F){#run it once
-      
-      #########################
-      #In case of other SSPs, we are starting with SSP2's statespace and changing it
-      eaprdt <- readxl::read_xlsx("../../ruban new pnas V1/education/ass_edu_ssps, V2_ms.xlsx")
-      setDT(eaprdt)
-      setnames(eaprdt,c("edu...13","agefin","yr"),c("edu","age","period"))
-      
-      for(SSP.name in  paste0("SSP",1:5)[-1]) {  
-        
-        # temp.eapr <- melt(ass.edu.dt,id.vars = c("period","sex","age","edu","var","areasex"),variable.name = "region")
-        
-        # eaprdt[,unique(edu)]
-        # eaprdt[residence=="Rural"&sex=="female"&ID=="IN.WB"&agegr==0]
-        # 
-        if(SSP.name=="SSP4") eaprdt[edu%in%c("e12","e23","e34"),eapr.ssp4 := eapr.ssp4*0.9]
-        
-        # xx <- eaprdt[residence=="Rural"&sex=="female"&ID=="IN.BR"&edu=="e23"]
-        # eaprdt[,pattern:=paste(sex,age,edu,ID,tolower(residence),sep="_")]
-        # var.sel = c("period","pattern",paste0("eapr.",tolower(SSP.name)))
-        # eaprdt <- eaprdt[,..var.sel]
-        # setnames(eaprdt,paste0("eapr.",tolower(SSP.name)),"eapr")
-        # ass.edu.dt[eaprdt,on = .(period,pattern),value:=eapr]
-        # xy <- ass.edu.dt[residence=="rural"&sex=="female"&region=="IN.BR"&edu=="e23"]
-        #########################
-        
-        
-        var.sel = c("period","sex","age","edu","ID","residence",paste0("eapr.",tolower(SSP.name)))
-        ieapr.final.col <- eaprdt[region!="india"&residence!="Total"][,..var.sel
-        ][,region:=paste(ID,tolower(residence),sep="_")
-        ][,`:=`(ID=NULL,residence=NULL)][,setnames(.SD,c("period","age",paste0("eapr.",tolower(SSP.name))),c("Time","agest","eapr"))
-        ][,`:=`(sex=substr(sex,1,1),edu=substr(edu,1,2),Time=Time+1)][,tob := Time-agest-5]
-        
-        
-        
-        # ##edu (turn these into EAPRs) - problem with the data
-        # ieapr.final.col <- data1%>%rename(Time=period)%>%
-        #   filter(var=="eapr"&!(edu%in%c("e24","e35","e46"))&age>10)%>%
-        #   gather(region,eapr,contains("_"))%>%select(-var)%>%
-        #   mutate(sex=substr(sex,1,1),edu=substr(edu,1,2),Time=Time+1,tob = Time-age-5)%>%
-        #   rename(agest=age)
-        # setDT(ieapr.final.col)
-        # 
-        # ieapr.2106 <- copy(ieapr.final.col)[Time==2106]
-        # 
-        # for(i in 1:3){
-        #   ieapr.xxx <- copy(ieapr.2106)[,Time:=Time+5*i][,tob:=tob+5*i]  
-        #   ieapr.final.col <- rbind(ieapr.final.col,ieapr.xxx)
-        #   }
-        # 
-        # 
-        # setDT(ieapr.final.col)
-        
-        
-        age.ult.edu <- c(15,15,20,25,30)
-        names(age.ult.edu) <- edunames[-nedu]
-        age.ult.edu.dtt <-as.data.table(data.frame(edu=edunames[-nedu],agest = age.ult.edu))
-        
-        fun.eapr.fill <- function(idf,iby){
-          
-          idf <<- idf
-          iby <<- iby
-          # stop("..")
-          i <<- i + 1
-          print(i)
-          isex = iby$sex
-          icc = iby$region
-          
-          {#get the baseline ratios (or eapr!!) from observations
-            
-            t = 2011 #base_year period
-            iprop.base <- copy(popdt)[Time==t&region==icc&sex==isex,
-            ][,setnames(.SD,"pop","value")
-            ][,by=.(agest),value:=prop.table(.SD$value)]#%>%spread(edu,pop)
-            # iprop.base.test <-iprop.base[agegr<10][,value:=round(value*100,0)]%>%spread(edu,value)
-            ieapr <- iprop.base[,eapr(.SD),by=.(agest)][,tob:=Time-agest-5]
-            
-            # placeholder
-            iprop.edu.lower.age <- data.table(edu=edunames[c(4,5,5,6,6,6)-1], #origin_edu
-                                              agest=c(15,15,20,15,20,25),
-                                              prop=0)
-            
-            
-            #eapr might be changing with cohort. but prop might not
-            #e.g. e4 - 15-19 (agegr = 4)
-            irow = 2
-            for(irow in 1:nrow(iprop.edu.lower.age)){
-              
-              #ieapr
-              iedu.lm = iprop.edu.lower.age[irow,edu]
-              #prop
-              iedu.prop <-educodes[match(iedu.lm,educodes)+1]
-              
-              #active age
-              iagest = iprop.edu.lower.age[irow,agest]
-              #active age's itob
-              itob <- t-iagest-5
-              
-              #ult age
-              iult.age = age.ult.edu[iedu.lm]
-              
-              #cumprop of at least iedu.prop   e4+ [15, eapr3] e5+ [15, eapr4]
-              cumprop_agest <- iprop.base[agest==iagest&
-                                            edu%in%educodes[match(iedu.prop,educodes):6],sum(value)]
-              # 84% with e4 at age 15_19
-              # 51% with e5 at age 15_19
-              
-              #check if the itob is one of the tobs ending with 0 or 5
-              
-              ieapr_ult <- copy(idf)[tob==itob & agest<=iult.age]
-              #Find ultimate value for itob at age 20-24
-              icumprop.ult.edu<- ieapr_ult[eapr>0&edu%in%educodes[1:(match(iedu.lm,educodes))]][,prod(eapr)]
-              #85% with e4 at age 20_24
-              #66% with e5 at age 25_29
-              
-              iprop.agest_edu <- cumprop_agest/icumprop.ult.edu
-              
-              if(iprop.agest_edu>=1) iprop.agest_edu = 1
-              iprop.edu.lower.age[edu==iedu.lm&agest==iagest,prop:=iprop.agest_edu]
-              
-              # iprop.edu.lower.age[,eapr:= paste0(edu,as.numeric(substr(edu,2,2))+1)]
-              
-            }#cal.prop_in_lower_ages
-            
-            
-            
-          }#cal.prop_in_lower_ages
-          #now get me all the assumptions
-          
-          
-          #for each cohort, we get their eaprs for younger ages
-          
-          # #what do we know at the time of max(int)
-          # t = t-t%%5
-          
-          #those 25_29 years and younger.. needs to be projected until 30-34
-          cumprop.proj.col <- NULL
-          
-          #t+5
-          
-          # itob = 1981# - Why 1980??
-          # #agest at time 2020 for each cohort
-          # itob = itob+5
-          for(itob in seq(t-25-5,2091,by=5)){
-            
-            # if(itob == 1995) stop()
-            
-            iage.t = t-itob-5 #<30 for the projection
-            
-            istep.age = min((30-iage.t)/5,4)
-            
-            #30 start with ultimate eduage for each cohort
-            if(istep.age > 0) {
-              #start with ultimate eaprs at age 30-34
-              idf1 <- copy(idf)[tob==itob&eapr>0][,agest:=30] #ulti_overall_age 
-              
-              idf1.missing <- copy(ieapr)[tob==itob&!(edu%in%unique(idf1$edu))
-              ][,setnames(.SD,"value","eapr")][,.(Time,agest,edu,eapr,tob)][,agest:=30]
-              
-              idf1 <- rbind(idf1.missing,idf1)
-              
-              idf1 <- idf1[,time:=itob+agest+5 #correspnding time tob = time - age -5
-              ][,cumprop:=cumprod(eapr)
-              ][,.(edu,tob,agest,time,cumprop)]
-              
-              cumprop.proj.col <- rbind(cumprop.proj.col,idf1)
-              
-              istep.age = istep.age-1
-              #correcting for probable error
-              #here we adjust - such that the cumprop for lower education is slightly higher than the cumprop of the next higher education 
-              if(any(idf1[,diff(.SD$cumprop)]>0)){
-                for (j in 1:4){
-                  if(idf1[j,cumprop] < idf1[j+1,cumprop]) idf1[j,cumprop] <-  idf1[j+1,cumprop]+.0001
-                }
-              }
-              
-              #25 [and then apply the proportions for younger ages..]
-              i = 0
-              while(istep.age>0){
-                
-                i = i + 1
-                
-                #age 
-                idf1x <- copy(idf1)[,agest:=agest-5*i #go back one age
-                ][,time:=time-5*i #go back one time
-                ][iprop.edu.lower.age,on=.(edu,agest),cumprop:=cumprop*prop]
-                
-                if(any(idf1x[,diff(.SD$cumprop)]>0)){
-                  #here we adjust - such that the cumprop for lower education is slightly higher than the cumprop of the next higher education 
-                  for (j in 1:4){
-                    if(idf1x[j,cumprop] < idf1x[j+1,cumprop]) idf1x$cumprop[j] <-  idf1x[j+1,cumprop]+.0001
-                  }
-                  
-                } 
-                
-                cumprop.proj.col <- rbind(cumprop.proj.col,idf1x)
-                istep.age = istep.age-1
-              }
-            }#istep.age >0
-            
-          }#for itob
-          
-          
-          # cumprop.proj.col[,table(agest,time)]
-          
-          
-          # #15-34
-          # idf_1534 <- cumprop.proj.col[time==2016]
-          # 
-          # if(min(idf_1534$tob)=="Inf") stop()
-          # 
-          # #35+
-          # tob_3034_in2020 <- idf_1534[,min(tob)]
-          # idf_35p <- copy(idf)[tob<tob_3034_in2020][,eapr:=un_logit(y)][
-          #   ,cumprop:=cumprod(.SD$eapr),by=.(agest)][,.(edu,tob,agest,time,cumprop)]
-          # # idf_35p[agest==35]
-          # cumprop.proj.int.col <- rbind(cumprop.proj.col,idf_35p)
-          # 
-          # #check
-          # fun.fix.negative <- function(dfx,iby){
-          #   # dfx <<- dfx
-          #   # iby <<- iby
-          #   # stop()
-          #   if(any(dfx[,diff(.SD$cumprop)]>0)) stop()
-          # }
-          # cumprop.proj.int.col[,fun.fix.negative(dfx=.SD,iby=.BY),by=.(tob,agest)]
-          # 
-          # 
-          # 
-          # cumprop.proj.int.col <- rbind(cumprop.proj.int.col,
-          #                               if(t<2020)ieapr.base.test.save[,tob:=time-agest-5])
-          # 
-          return(cumprop.proj.col)
-          
-        }#fun.eapr.fill
-        
-        #collect for 2020 (input_prop) and for the future (15_30, 2016 onwards)
-        i=0; icumprop.final.col <- ieapr.final.col[,fun.eapr.fill(.SD,iby=.BY),by=.(region,sex)]
-        
-        #check i = 400
-        save(icumprop.final.col,file = paste0("../data/education/icumprop.final.col ",SSP.name,".rda"))
-      }#ssps
-      
-    }#run it once
-    # rm(icumprop.final.col)
+write_csv(propdt2,"data/education/propdt2.csv")
+
+
+# migration ---------------------------------------------------------------
+data5<-read_csv("data/migration/full_mig_2060.csv")
+
+data5 %<>%
+  mutate(region = case_when(area=="RO"~11,area=="AC"~12,area=="AM"~13,area=="RR"~14,area=="PA"~15,area=="AP"~16,area=="TO"~17,
+                            area=="MA"~21,area=="PI"~22,area=="CE"~23,area=="RN"~24,area=="PB"~25,area=="PE"~26,area=="AL"~27,area=="SE"~28,area=="BA"~29,
+                            area=="MG"~31,area=="ES"~32,area=="RJ"~33,area=="SP"~35,
+                            area=="PR"~41,area=="SC"~42,area=="RS"~43,
+                            area=="MS"~50,area=="MT"~51,area=="GO"~52,area=="DF"~53)) %>% 
+  separate(age, c("agest", "end")) %>% mutate(agest = as.numeric(agest)) %>%
+  separate(year, c("Time", "endtime")) %>% mutate(Time = as.numeric(Time)) %>%
+  mutate(edu = case_when(education=="No Education"~"e1",
+                         education=="Incomplete Primary"~"e2",
+                         education=="Primary"~"e3",
+                         education=="Lower Secondary"~"e4",
+                         education=="Upper Secondary"~"e5",
+                         education=="Post Secondary"~"e6"))%>% 
+  mutate(sex= case_when(sex=="Female"~"f", sex=="Male"~"m"))%>% 
+  mutate(region = as.character(region)) %>% 
+  select(region, Time, sex, edu, agest, prop_in, prop_out, inmigfinal, outmigfinal)
+
+idmrdt2 <- full_join(idmrdt, data5) %>% #ATTENTION: PROPORTIONS OF MIGRANTS RELATED TO THE TOTAL OF THE AREA. NOT MIGRATION RATES!
+  select(region, Time, sex, edu, agest, prop_in, inmigfinal)
+
+odmrdt2 <- full_join(odmrdt, data5) %>% #ATTENTION: PROPORTIONS OF MIGRANTS RELATED TO THE TOTAL OF THE AREA. NOT MIGRATION RATES!
+  select(region, Time, sex, edu, agest, prop_out, outmigfinal)
+
+write_csv(idmrdt2,"data/migration/idmrdt2.csv")
+write_csv(odmrdt2,"data/migration/odmrdt2.csv")
+
     
-    load(file = paste0("../data/education/icumprop.final.col ",SSP.name,".rda"))
-    
-    
-    iprop.final.col <- icumprop.final.col%>%spread(edu,cumprop)
-    iprop.final.col <-copy(iprop.final.col)[,`:=`(e6=e5,e5=e4-e5,e4=e3-e4,e3=e2-e3,e2=e1-e2,e1=1-e1)]
-    
-    #check for 
-    iprop.final.col[,tot:=rowSums(.SD),.SDcols = e1:e6]
-    any(abs(iprop.final.col[,tot]-1)>0.00001)
-    iprop.final.col[,tot:=NULL]
-    
-    # save(iprop.final.col,file="../data/education/iprop.final.col.rda")
-    
-    iprop.final.col <- iprop.final.col%>%gather(edu,prop,e1:e6)
-    setDT(iprop.final.col)
-    
-    propdt[,prop:=0][iprop.final.col[,setnames(.SD,"time","Time")],on=id.cols,prop:=i.prop]
-    #check
-    propdt[Time==2016,summary(prop)]
-    
-    
-    # migration ---------------------------------------------------------------
-    #migration input
-    if(F) source("mig calc.r") #NOTE:  smoothing validation is needed!!
-    
-    migOD_AG <- read.csv("../data/migration/mig final all variables 2023402.csv")
-    setDT(migOD_AG)
-    migOD_AG[,sum(mig04)]
-    
-    migOD_AG[,`:=`(dest=tolower(dest),orig=tolower(orig))][]
-    
-    migOD_AG[, c("cstnm", "cresi") := tstrsplit(dest, "_", fixed=TRUE)]
-    migOD_AG[, c("lstnm", "lresi") := tstrsplit(orig, "_", fixed=TRUE)]
-    migOD_AG[,cstnm:=stcodes$HASC[match(cstnm,tolower(stcodes$area0))]]
-    migOD_AG[,lstnm:=stcodes$HASC[match(lstnm,tolower(stcodes$area0))]]
-    
-    migOD_AG <- migOD_AG[,.(lstnm,lresi,cstnm,cresi,agegr,sex,mrate.pred)][
-      ,`:=`(dest=paste(cstnm,cresi,sep='_'),region=paste(lstnm,lresi,sep='_'))][
-        ,agest:=(agegr-1)*5
-      ][region==dest,mrate.pred:=0
-      ][,.(region,dest,sex,agest,mrate.pred)
-      ][,agest:=agest-5]#to match age at the beginning of the projection period
-    # %>%spread(dest,mrate.pred)
-    
-    #int.mig
-    issp.dom.var <- ssp.var[ssp==substr(SSP.name,4,4),.(region,dommig)]
-    setnames(issp.dom.var,"dommig", "variant")
-    print("will deal when we reach the point ")
-    
-    dom.ssp = data.table(expand.grid(region = issp.dom.var$region,Time = seq(2011,2096,5)))
-    dom.ssp[issp.dom.var,on=.(region),variant:=variant][
-      intmig.var,on=.(variant,Time),ssp.adj:=ssp.adj][
-        ,`:=`(variant=NULL)]     
-    print("Dom will be changed during the projection")
-    
-    # period    sex age edu      origin destination       value
-    # 1:   2010   male   0  e1 IN.AN_urban IN.AN_urban 9.275084016
-    # 2:   2010   male   0  e1 IN.AD_urban IN.AN_urban 0.054831155
-    # 3:   2010   male   0  e1 IN.AR_urban IN.AN_urban 0.007690488
-    # 4:   2010   male   0  e1 IN.AS_urban IN.AN_urban 0.005288164
-    # 5:   2010   male   0  e1 IN.BR_urban IN.AN_urban 0.008869242
-    # ---                                                          
-    #   907376:   2010 female  70  e6 IN.TR_rural       World 0.000000000
-    # 907377:   2010 female  70  e6 IN.UT_rural       World 0.000000000
-    # 907378:   2010 female  70  e6 IN.UP_rural       World 0.000000000
-    # 907379:   2010 female  70  e6 IN.WB_rural       World 0.000000000
-    # 907380:   2010 female  70  e6       World       World 0.000000000
-    
-    #idmrdt
-    id.cols.here <- intersect(id.cols,names(imrdt))
-    
-    idmrdt[,imr:=0]#[setDT(input),  imr:=mrate, on = id.cols.here]
-    
-    #edmrdt
-    id.cols.here <- intersect(id.cols,names(imrdt))
-    imrdt[,imr:=0]#[setDT(input),  imr:=mrate, on = id.cols.here]
-    
-    
-    
-    if(F){ 
-      data2<-read_csv("../data/input_data_pnas/india_AGSRE_Baseline_mig.csv")#
-      setDT(data2)
-      # 2001 rates
-      # age 0 - 70+ overall_age 
-      # 0 international migration..
-      # mrate per 1000
-      data2 <- melt(data2[,var:=NULL],id.vars = c("period","sex","age","edu","origin"),variable.name = "destination")
-    }
-    
-    #imr
-    #imrdt
-    id.cols.here <- intersect(id.cols,names(imrdt))
-    imrdt[,imr:=0]#[setDT(input),  imr:=mrate, on = id.cols.here]
-    
-    #emrdt
-    ##replace with new data ?? MnM has provided 16 countries
-    id.cols.here <- intersect(id.cols,names(emrdt))
-    emrdt[,emr:=0]#[setDT(input), emr:=mrate, on = id.cols.here]
-    
-    
-    #urbanization variable ...
-    # !!! Maybe it is easier to run the projection directly for other SSPs !!!
-    #education component
-    
+
     #SSPs
     
     
-    # reclass -----------------------------------------------------------------
-    data1[,unique(var)]
-    reclass.dt <-    data1[var%in%c("reclasstr","gap","perural")]
-    
-    if (nrow(reclass.dt) > 0) {
-      m.vars <- names(reclass.dt)[-c(1:which(names(reclass.dt) == "var"))]
-      reclass.dt <- melt(reclass.dt, measure.vars = m.vars)
-      reclass.dt <- reclass.dt[grep("rural", reclass.dt$variable), ]
-      reclass.dt <- dcast(reclass.dt, factor(variable) ~ var, value.var = "value")
-      names(reclass.dt)[1] <- "region"
-      reclass.dt$state <- sapply(reclass.dt$region, function(x) sub("_rural", "", x))
-      regions.oldtr <- reclass.dt[reclasstr > 0.05 & perural > 0.4 | sapply(reclass.dt$reclasstr, function(x) identical(all.equal(x, 0), TRUE)), 
-                                  .(state = state, reclasstr)]
-      # if (any(reclass.dt$reclasstr > 0) & is.not.null(reclass.model)) {
-      #   load(paste("input_models/", reclass.model, sep = ""))
-      # }
-    } else {
-      rm(reclass.dt)
-    }
-    
-    #reclass
-    issp.reclass.var <- ssp.var[ssp==substr(SSP.name,4,4),.(region,reclass)]
-    setnames(issp.reclass.var,"reclass", "variant")
-    print("will deal with reclass when we reach the point ")
-    
-    reclass.ssp = data.table(expand.grid(region = issp.reclass.var$region,Time = seq(2011,2096,5)))
-    reclass.ssp[issp.reclass.var,on=.(region),variant:=variant][
-      reclass.var,on=.(variant,Time),ssp.adj:=ssp.adj][
-        ,`:=`(variant=NULL)]     
-    
-    area.vars = c("state","residence")
-    reclass.ssp <- unique(reclass.ssp[, c(area.vars) := tstrsplit(region, "_")][,.(Time,ssp.adj)])
-    print("Reclassification during the projection")
-    
-    
+  
     # NON Baseline ------------------------------------------------------------
     
     
